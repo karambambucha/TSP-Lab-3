377cfa48c2d021625fec439bd0a19e8f2dfd1b20
karambambucha <TSMFDR@yandex.ru>
2023-04-22 21:30:30 +0700

    Добавьте файлы проекта.

---
+++ TSP-Lab-3/Form1.Designer.cs
@@ -0,0 +1,484 @@
+﻿namespace TSP_Lab_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nud1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nud2 = new System.Windows.Forms.NumericUpDown();
+            this.nud3 = new System.Windows.Forms.NumericUpDown();
+            this.nud4 = new System.Windows.Forms.NumericUpDown();
+            this.nud5 = new System.Windows.Forms.NumericUpDown();
+            this.nud6 = new System.Windows.Forms.NumericUpDown();
+            this.nud7 = new System.Windows.Forms.NumericUpDown();
+            this.nud8 = new System.Windows.Forms.NumericUpDown();
+            this.nud9 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.numericUpDown10 = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nud1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud9)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nud1
+            // 
+            this.nud1.DecimalPlaces = 3;
+            this.nud1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nud1.Location = new System.Drawing.Point(53, 27);
+            this.nud1.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud1.Name = "nud1";
+            this.nud1.Size = new System.Drawing.Size(64, 20);
+            this.nud1.TabIndex = 0;
+            this.nud1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Дождь";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Снег";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ясно";
+            // 
+            // nud2
+            // 
+            this.nud2.DecimalPlaces = 2;
+            this.nud2.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nud2.Location = new System.Drawing.Point(123, 27);
+            this.nud2.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud2.Name = "nud2";
+            this.nud2.Size = new System.Drawing.Size(64, 20);
+            this.nud2.TabIndex = 4;
+            this.nud2.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            131072});
+            // 
+            // nud3
+            // 
+            this.nud3.DecimalPlaces = 2;
+            this.nud3.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nud3.Location = new System.Drawing.Point(193, 27);
+            this.nud3.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud3.Name = "nud3";
+            this.nud3.Size = new System.Drawing.Size(64, 20);
+            this.nud3.TabIndex = 5;
+            this.nud3.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            131072});
+            // 
+            // nud4
+            // 
+            this.nud4.DecimalPlaces = 2;
+            this.nud4.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nud4.Location = new System.Drawing.Point(53, 53);
+            this.nud4.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud4.Name = "nud4";
+            this.nud4.Size = new System.Drawing.Size(64, 20);
+            this.nud4.TabIndex = 6;
+            this.nud4.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            // 
+            // nud5
+            // 
+            this.nud5.DecimalPlaces = 2;
+            this.nud5.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nud5.Location = new System.Drawing.Point(123, 53);
+            this.nud5.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud5.Name = "nud5";
+            this.nud5.Size = new System.Drawing.Size(64, 20);
+            this.nud5.TabIndex = 7;
+            // 
+            // nud6
+            // 
+            this.nud6.DecimalPlaces = 2;
+            this.nud6.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nud6.Location = new System.Drawing.Point(193, 55);
+            this.nud6.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud6.Name = "nud6";
+            this.nud6.Size = new System.Drawing.Size(64, 20);
+            this.nud6.TabIndex = 8;
+            this.nud6.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            // 
+            // nud7
+            // 
+            this.nud7.DecimalPlaces = 2;
+            this.nud7.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nud7.Location = new System.Drawing.Point(53, 79);
+            this.nud7.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud7.Name = "nud7";
+            this.nud7.Size = new System.Drawing.Size(64, 20);
+            this.nud7.TabIndex = 9;
+            this.nud7.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            131072});
+            // 
+            // nud8
+            // 
+            this.nud8.DecimalPlaces = 2;
+            this.nud8.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nud8.Location = new System.Drawing.Point(123, 79);
+            this.nud8.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud8.Name = "nud8";
+            this.nud8.Size = new System.Drawing.Size(64, 20);
+            this.nud8.TabIndex = 10;
+            this.nud8.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            131072});
+            // 
+            // nud9
+            // 
+            this.nud9.DecimalPlaces = 2;
+            this.nud9.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nud9.Location = new System.Drawing.Point(193, 81);
+            this.nud9.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud9.Name = "nud9";
+            this.nud9.Size = new System.Drawing.Size(64, 20);
+            this.nud9.TabIndex = 11;
+            this.nud9.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(66, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Дождь";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(137, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Ясно";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(209, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Снег";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(8, 107);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(249, 89);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выберите изначальное состояние";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 67);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(49, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "Снег";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 44);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(51, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ясно";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(60, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "Дождь";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown10
+            // 
+            this.numericUpDown10.Location = new System.Drawing.Point(123, 202);
+            this.numericUpDown10.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown10.Name = "numericUpDown10";
+            this.numericUpDown10.Size = new System.Drawing.Size(64, 20);
+            this.numericUpDown10.TabIndex = 16;
+            this.numericUpDown10.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown10.ValueChanged += new System.EventHandler(this.numericUpDown10_ValueChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 204);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(93, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Количество дней";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(263, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(525, 426);
+            this.richTextBox1.TabIndex = 18;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 228);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 43);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Узнать прогноз погоды на 2 дней(-я)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.numericUpDown10);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nud9);
+            this.Controls.Add(this.nud8);
+            this.Controls.Add(this.nud7);
+            this.Controls.Add(this.nud6);
+            this.Controls.Add(this.nud5);
+            this.Controls.Add(this.nud4);
+            this.Controls.Add(this.nud3);
+            this.Controls.Add(this.nud2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nud1);
+            this.Name = "Form1";
+            this.Text = "Марковские цепи";
+            ((System.ComponentModel.ISupportInitialize)(this.nud1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud9)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown10)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nud1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nud2;
+        private System.Windows.Forms.NumericUpDown nud3;
+        private System.Windows.Forms.NumericUpDown nud4;
+        private System.Windows.Forms.NumericUpDown nud5;
+        private System.Windows.Forms.NumericUpDown nud6;
+        private System.Windows.Forms.NumericUpDown nud7;
+        private System.Windows.Forms.NumericUpDown nud8;
+        private System.Windows.Forms.NumericUpDown nud9;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.NumericUpDown numericUpDown10;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TSP-Lab-3/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TSP_Lab_3
+{
+    public partial class Form1 : Form
+    {
+        private WeatherTypes preferredWeather;
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = "Узнать прогноз погоды на " + numericUpDown10.Value + " дней(-я)";
+        }
+        enum WeatherTypes
+        {
+            Rainy, Sunny, Snowy
+        }
+        double[,] Multiplication(double[,] a, double[,] b)
+        {
+            if (a.GetLength(1) != b.GetLength(0)) throw new Exception("Матрицы нельзя перемножить");
+            double[,] r = new double[a.GetLength(0), b.GetLength(1)];
+            for (int i = 0; i < a.GetLength(0); i++)
+                for (int j = 0; j < b.GetLength(1); j++)
+                    for (int k = 0; k < b.GetLength(0); k++)
+                        r[i, j] += a[i, k] * b[k, j];
+            return r;
+        }
+        double[,] MatrixPower(double[,] a, int power)
+        {
+            double[,] res = a;
+            for (int i = 0; i < power; i++)
+            {
+                res = Multiplication(res, a);
+                richTextBox1.Text += $"Прогноз погоды через {i + 1} дней(-я): \n";
+                richTextBox1.Text += Print(res);
+            }
+            return res;
+        }
+        string Print(double[,] a)
+        {
+            var tableStr = new StringBuilder();
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    tableStr.Append($"[{string.Format("{0:f10}", a[i, j])}]\t");
+                }
+                if (Array.IndexOf(Enum.GetValues(preferredWeather.GetType()), preferredWeather) == i)
+                    tableStr.Append("(*)");
+                tableStr.Append("\n");
+            }
+
+            tableStr.Append("\n");
+            return tableStr.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (nud1.Value + nud2.Value + nud3.Value != 1
+                 || nud4.Value + nud5.Value + nud6.Value != 1
+                 || nud7.Value + nud8.Value + nud9.Value != 1)
+                {
+                    throw new Exception("Сумма одной или нескольких строк не равна 1!");
+                }
+                else
+                {
+                    if (radioButton1.Checked)
+                        preferredWeather = WeatherTypes.Rainy;
+                    else if (radioButton2.Checked)
+                        preferredWeather = WeatherTypes.Sunny;
+                    else if (radioButton3.Checked)
+                        preferredWeather = WeatherTypes.Snowy;
+                    double[,] probabilityTable = { { (double)nud1.Value, (double)nud2.Value, (double)nud3.Value },
+                                                   { (double)nud4.Value, (double)nud5.Value, (double)nud6.Value },
+                                                   { (double)nud7.Value, (double)nud8.Value, (double)nud9.Value } };
+                    richTextBox1.Text = "";
+                    MatrixPower(probabilityTable, (int)numericUpDown10.Value);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void numericUpDown10_ValueChanged(object sender, EventArgs e)
+        {
+            button1.Text = "Узнать прогноз погоды на " + numericUpDown10.Value + " дней(-я)";
+        }
+    }
+}
